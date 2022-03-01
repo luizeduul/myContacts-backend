c7596b09ba1e28d08fb921364ba4a6d2e92b17d6
Luiz Uliana <luizeduardr@gmail.com>
2022-03-01 18:23:35 -0300

    Initial commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,100 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    return response.json(contact);
+  }
+
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({
+        error: 'Name is required',
+      });
+    }
+
+    const contactsExists = await ContactsRepository.findByEmail(email);
+
+    if (contactsExists) {
+      return response.status(400).json({
+        error: 'This email already in use',
+      });
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.json(contact);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({
+        error: 'Name is required',
+      });
+    }
+
+    const exists = await ContactsRepository.findById(id);
+
+    if (!exists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({
+        error: 'This email already in use',
+      });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.json(contact);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    await ContactsRepository.deleteById(id);
+
+    return response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
